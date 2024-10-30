5fd7df88ba17b89d067d6cba4e1ffdf191b5427a
TechnoRoss <TechnoRoss@users.noreply.github.com>
2024-10-30 11:13:10 -0500

    initial commit

---
+++ sample-app/global.js
@@ -0,0 +1,6 @@
+window._env_ = {
+  pingOneDomain: "com",
+  companyId: "7b3378ed-3d11-4c67-95fe-8fc78eaa129b",
+  apiKey: "e27d44dcede73ca2f603637ee0df9905700c757985549b652bae466dda0fe2a8dce0592652e4fd2b11083a6f25440405fa4d056cad1bcd339c43a14de707a532e3e16b2aaea3cdbc9c57ac080da06d552b7dad341e2f1cfce3ff6447ae4b06af467513e47df6e426a1651a37d59a2e4e39f6144c9dbfc68dcd04c5a00586f2bf",
+  policyId: "12c3b93dbb68a5d4bbde1060eb709fad"
+};
